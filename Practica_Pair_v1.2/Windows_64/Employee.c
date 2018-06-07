d73ca70086f29c2772f7b523e0d1597d1ff6f8f6
EzequielSegota <36193787+EzequielSegota@users.noreply.github.com>
2018-06-07 11:33:53 -0300

    Add files via upload

---
+++ Practica_Pair_v1.2/Windows_64/Employee.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+
+int employee_compare(void* pEmployeeA,void* pEmployeeB)
+{
+    Employee* e1;
+    Employee* e2;
+    int retorno=-5;
+    //Casteo de void a tipo struct
+    e1=(Employee*)pEmployeeA;
+    e2=(Employee*)pEmployeeB;
+
+    return (stricmp(e1->name, e2->name));
+}
+
+
+void employee_print(Employee* this)
+{
+    printf("ID: %d\n",this->id);
+    printf("Nombre: %s\n",this->name);
+    printf("Apellido: %s\n",this->lastName);
+}
+
+
+Employee* employee_new(void)
+{
+
+    Employee* returnAux = NULL;
+
+    returnAux=(Employee*)malloc(sizeof(Employee));
+
+
+    return returnAux;
+
+}
+
+void employee_delete(Employee* this)
+{
+
+
+}
+
+int employee_setId(Employee* this, int id)
+{
+
+    return 0;
+
+}
+
+int employee_getId(Employee* this)
+{
+
+    return 0;
+
+}
+
+
---
+++ Practica_Pair_v1.2/Windows_64/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+/****************************************************
+    Menu:
+        1. Parse del archivo data.csv
+        2. Listar Empleados
+        3. Ordenar por nombre
+        4. Agregar un elemento
+        5. Elimina un elemento
+        6. Listar Empleados (Desde/Hasta)
+*****************************************************/
+
+
+int main()
+{
+//creacion de lista
+    ArrayList* lista;
+    lista=al_newArrayList();
+//Creacion de empleado
+    Employee* e1;
+    Employee* e2;
+    Employee* eAux;
+    e1=employee_new();
+    e2=employee_new();
+    int i;
+//Asignacion
+    e1->id=666;
+
+    strcpy(e1->name,"Auan");
+    strcpy(e1->lastName,"Gomez");
+    e1->isEmpty=1;
+
+    al_add(lista,e1);//Agrega al array de lista el empleado
+    al_add(lista,e2);
+    int espacioOcupado =al_len(lista);//Cantidad de espacio ocupado, devuelve el size
+    printf("El espacio ocupado es:%d\n",espacioOcupado);
+//Creo empleado auxiliar
+    //Movido arriba
+    //eAux=(Employee*)al_get(lista,0);//Obtiene elemento de la lista en posicion 0
+//Imprimo el empleado de la posiciòn cero
+    //employee_print(eAux);
+
+    //Creo otro empleado y lo agrego a la lista
+     //movido arriba
+     e2->id=123;
+     strcpy(e2->name,"Zulio");
+     strcpy(e2->lastName,"Cortazar");
+     e2->isEmpty=1;
+
+
+
+/*
+     for(i=0;i<espacioOcupado;i++)
+     {
+         eAux=(Employee*)al_get(lista,i);
+         employee_print(eAux);
+     }
+*/
+    //ORDENAR
+
+    al_sort(lista,employee_compare,0);
+
+    for(i=0;i<espacioOcupado;i++)
+    {
+         eAux=(Employee*)al_get(lista,i);
+         employee_print(eAux);
+    }
+
+    return 0;
+}
