4e87fa39037291e149ec16eb13d69665dbbf10a0
alancama <42609039+alancama@users.noreply.github.com>
2018-11-07 10:25:37 -0300

    terminar parcial

---
+++ CAMA_ALAN_1A_PP/consultas.h
@@ -0,0 +1,15 @@
+#ifndef CONSULTAS_H_INCLUDED
+#define CONSULTAS_H_INCLUDED
+#include "juegos.h"
+#include "alquileres.h"
+
+void importesPromedio(eJuego juegos[],int tamJuego,eAlquiler alquileres[],int tamAlquiler);
+void abmConsultas(eAlquiler alquiler[],int tamAlquiler,eJuego juegos[],int tamJuego,eCliente clientes[],int tamClientes);
+void PromedioQueNoSupera(eJuego juegos[],int tamJuegos,eAlquiler alquileres[],int tamAlquiler);
+void listadoDeclientesJuegoDeter(eCliente clientes[],int tamCliente,eJuego juegos[],int tamJuego);
+void listadoJuegosMemosAlquilados(eAlquiler alquileres[],int tamAlquileres,eJuego juegos[],int tamJuegos);
+void listadoClientesOrdenados(eCliente clientes [],int tamCliente);
+void listadoJuegosOrdenados(eJuego juegos[],int tamJuegos);
+void listarJuegosxCliente(eAlquiler alquileres[], int tamAlqui, eJuego juegos[], int tamJuegos, eCliente cliente[],  int tamClientes);
+int seleccionarCliente(eCliente cliente[], int lenClien);
+#endif // CONSULTAS_H_INCLUDED
